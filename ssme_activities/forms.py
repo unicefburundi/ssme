86a0ea3b4dc936a579f857ffde1a32b37609d214
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2015-10-04 22:49:16 +0200

    stabilized

---
+++ ssme_activities/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from authtools.forms import UserCreationForm
+
+class UserCreationForm(UserCreationForm):
+    """
+    A UserCreationForm with optional password inputs.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].required = False
+        self.fields['password2'].required = False
+        # If one field gets autocompleted but not the other, our 'neither
+        # password or both password' validation will be triggered.
+        self.fields['password1'].widget.attrs['autocomplete'] = 'off'
+        self.fields['password2'].widget.attrs['autocomplete'] = 'off'
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = super(UserCreationForm, self).clean_password2()
+        if bool(password1) ^ bool(password2):
+            raise forms.ValidationError("Fill out both fields")
+        return password2
