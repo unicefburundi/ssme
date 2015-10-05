8c2a666286b0ac18c26df8af13349b2a772c6e7a
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2015-10-05 15:49:49 +0200

    changed links and readded sqlite in db

--- ssme/settings.py
+++ ssme/settings.py
@@ -72,12 +72,12 @@ WSGI_APPLICATION = 'ssme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
+}
 
 
 # Internationalization
