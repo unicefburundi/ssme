4cf75db225796032b562b04575abe1546cafaae7
mbanje <mbanjejvernack@gmail.com>
2015-10-06 11:28:58 +0200

    Link Beneficiaries to CDS in models

--- ssme_activities/models.py
+++ ssme_activities/models.py
@@ -98,11 +98,19 @@ class Product(models.Model):
     unite_de_mesure = models.CharField(max_length=10)
 
 class CampaignBeneficiary(models.Model):
-    '''With this model, we will be able to define and identify beneficiaries for a given ssme campaign'''
-    campaign = models.ForeignKey(Campaign)
-    beneficairy = models.ForeignKey(Beneficiaire)
-    class Meta:
-        unique_together = ('campaign', 'beneficairy',)
+	'''With this model, we will be able to define and identify beneficiaries for a given ssme campaign'''
+	campaign = models.ForeignKey(Campaign)
+	beneficiary = models.ForeignKey(Beneficiaire)
+	order_in_sms = models.IntegerField()
+	
+	class Meta:
+		unique_together = ('campaign', 'beneficiary', 'order_in_sms',)
+
+class CampaignBeneficiaryCDS(models.Model):
+	campaign_beneficiary = models.ForeignKey(CampaignBeneficiary)
+	cds = models.ForeignKey(CDS)
+	population_attendu = models.IntegerField(null=True)
+	population_obtenue = models.IntegerField(null=True)
 
 class CampaignProduct(models.Model):
     '''With this model we will be able to define and identify concerned medecines for a given campaign'''
@@ -116,8 +124,7 @@ class CampaignBeneficiaryProduct(models.Model):
     category and quantity for each Beneficiary'''
     camapaign_beneficiary = models.ForeignKey(CampaignBeneficiary)
     campaign_product = models.ForeignKey(CampaignProduct)
-    dosage = models.FloatField()
-    order_in_sms = models.IntegerField()
+    dosage = models.FloatField(null = True)
 
 class Report(models.Model):
     '''In this model, we will store each report'''
@@ -128,7 +135,7 @@ class Report(models.Model):
     category = models.CharField(max_length=50)
 
 class ReportBeneficiary(models.Model):
-    campaign_product_beneficiary = models.ForeignKey(CampaignBeneficiaryProduct)
+    campaign_beneficiary = models.ForeignKey(CampaignBeneficiary)
     reception_date = models.DateField()
     received_number = models.IntegerField()
     report = models.ForeignKey(Report)
