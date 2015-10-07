3e2e68f4883389cee36dba4a6b69666e36e94bc4
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2015-10-07 10:51:52 +0200

    fixed get_by_level and layout fixes

--- ssme/views.py
+++ ssme/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from ssme_activities.models import Province, District, CDS
+from django.http import JsonResponse
 
 def landing(request):
     return render(request, 'landing_page.html')
+
+def get_by_level(request, level='CEN'):
+    results_dict = {}
+    results = None
+    if level == 'BPS':
+        results = Province.objects.all().order_by('name')
+    if level == 'BDS':
+        results = District.objects.all().order_by('name')
+    if level == 'CDS':
+        results = CDS.objects.all().order_by('name')
+    if level in ['CEN', '']  :
+        return JsonResponse([{'':'-----'}], safe=False)
+    for result in results:
+        results_dict[result.code] = result.name
+    return JsonResponse([results_dict], safe=False)
