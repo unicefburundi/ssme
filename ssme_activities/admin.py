ca76e9ce3340cc89463774e0f6a2cc31734a1a71
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2015-11-06 19:10:09 +0200

    added required admin filtering and fields

--- ssme_activities/admin.py
+++ ssme_activities/admin.py
@@ -7,23 +7,42 @@ from ssme_activities.models import  *
 from ssme_activities.forms import UserCreationForm
 from import_export import resources
 from import_export.admin import ExportMixin
+from django.contrib.admin import DateFieldListFilter
 
-class CampaignBeneficiaryProductAdmin(admin.ModelAdmin):
+
+class CampaignBeneficiaryProductResource(resources.ModelResource):
+    class Meta:
+        model = CampaignBeneficiaryProduct
+        fields = ('campaign_beneficiary__beneficiary__designation', 'campaign_product__product__name', 'dosage', 'pourcentage_attendu')
+
+class CampaignBeneficiaryProductAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = CampaignBeneficiaryProductResource
     list_display = ('campaign_beneficiary', 'campaign_product', 'dosage', 'pourcentage_attendu')
 
-class CampaignBeneficiaryAdmin(admin.ModelAdmin):
+class CampaignBeneficiaryResource(resources.ModelResource):
+    class Meta:
+        model = CampaignBeneficiary
+        fields = ('beneficiary__designation',  'pourcentage_attendu', 'order_in_sms')
+
+class CampaignBeneficiaryAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = CampaignBeneficiaryResource
     list_display = ('campaign','beneficiary','order_in_sms', 'pourcentage_attendu')
 
-class CampaignProductAdmin(admin.ModelAdmin):
+class CampaignProductResource(resources.ModelResource):
+    class Meta:
+        model = CampaignProduct
+
+class CampaignProductAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = CampaignProductResource
     list_display = ('campaign','product','order_in_sms')
 
-class CampaignCDSAResource(resources.ModelResource):
+class CampaignCDSResource(resources.ModelResource):
     class Meta:
         model = CampaignCDS
         fields = ('cds__name', 'population_cible', 'cds__district__name', 'cds__district__province__name')
 
 class CampaignCDSAdmin(ExportMixin, admin.ModelAdmin):
-    resource_class = CampaignCDSAResource
+    resource_class = CampaignCDSResource
     list_display = ('campaign', 'cds', 'population_cible', 'district', 'province')
     search_fields = ('cds__name', 'cds__district__name', 'cds__district__province__name')
 
@@ -33,9 +52,95 @@ class CampaignCDSAdmin(ExportMixin, admin.ModelAdmin):
     def province(self, obj):
         return obj.cds.district.province.name
 
-class ReportAdmin(admin.ModelAdmin):
-    search_fields = ('text', 'category')
-    list_display = ('text', 'category', 'cds')
+class ReportResource(resources.ModelResource):
+    class Meta:
+        model = Report
+        fields = ('text', 'concerned_date', 'category', 'cds__name', 'cds__district__name', 'cds__district__province__name')
+
+class ReportAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = ReportResource
+    search_fields = ('text', 'category', 'cds__name', 'cds__district__name', 'cds__district__province__name')
+    list_filter = ('concerned_date', 'reporting_date')
+    list_display = ('text', 'concerned_date', 'category', 'cds', 'district', 'province', )
+
+    def district(self, obj):
+        return obj.cds.district.name
+
+    def province(self, obj):
+        return obj.cds.district.province.name
+
+class ReportBeneficiaryResource(resources.ModelResource):
+    class Meta:
+        model = ReportBeneficiary
+        fields = ('campaign_beneficiary__beneficiary__designation', 'received_number', 'reception_date',  'report__cds__name', 'report__cds__district__name', 'report__cds__district__province__name')
+
+class ReportBeneficiaryAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = ReportBeneficiaryResource
+    search_fields = ( 'report__cds__name', 'report__cds__district__name', 'report__cds__district__province__name')
+    list_filter = ('reception_date', 'report__reporting_date')
+    list_display = ('beneficiary', 'received_number', 'reception_date',  'cds', 'district', 'province', )
+
+    def beneficiary(self, obj):
+        return obj.campaign_beneficiary.beneficiary.designation
+
+    def cds(self, obj):
+        return obj.report.cds.name
+
+    def district(self, obj):
+        return obj.report.cds.district.name
+
+    def province(self, obj):
+        return obj.report.cds.district.province.name
+
+
+class ReportProductReceptionResource(resources.ModelResource):
+    class Meta:
+        model = ReportProductReception
+        fields = ('received_quantity', 'reception_date',  'report__cds__name', 'report__cds__district__name', 'report__cds__district__province__name')
+
+class ReportProductReceptionAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = ReportProductReceptionResource
+    search_fields = ('campaign_product__product__name',  'report__cds__name', 'report__cds__district__name', 'report__cds__district__province__name')
+    list_filter = ('reception_date', 'report__reporting_date')
+    list_display = ('product', 'received_quantity', 'reception_date',  'cds', 'district', 'province', )
+
+    def product(self, obj):
+        return obj.campaign_product.product.name
+
+    def cds(self, obj):
+        return obj.report.cds.name
+
+    def district(self, obj):
+        return obj.report.cds.district.name
+
+    def province(self, obj):
+        return obj.report.cds.district.province.name
+
+class ReportProductRemainStockResource(resources.ModelResource):
+    class Meta:
+        model = ReportProductRemainStock
+        fields = ('remain_quantity', 'concerned_date',  'report__cds__name', 'report__cds__district__name', 'report__cds__district__province__name')
+
+class ReportProductRemainStockAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = ReportProductRemainStockResource
+    search_fields = ('campaign_product__product__name',  'report__cds__name', 'report__cds__district__name', 'report__cds__district__province__name')
+    list_filter = ('concerned_date', 'report__reporting_date')
+    list_display = ('product', 'remain_quantity', 'concerned_date',  'cds', 'district', 'province', )
+
+    def reports(self, obj):
+        return obj.report.reception_date
+
+    def product(self, obj):
+        return obj.campaign_product.product.name
+
+    def cds(self, obj):
+        return obj.report.cds.name
+
+    def district(self, obj):
+        return obj.report.cds.district.name
+
+    def province(self, obj):
+        return obj.report.cds.district.province.name
 
 User = get_user_model()
 
@@ -93,9 +198,9 @@ admin.site.register(CampaignBeneficiary, CampaignBeneficiaryAdmin)
 admin.site.register(CampaignProduct, CampaignProductAdmin)
 admin.site.register(CampaignBeneficiaryProduct, CampaignBeneficiaryProductAdmin)
 admin.site.register(Report, ReportAdmin)
-admin.site.register(ReportBeneficiary)
-admin.site.register(ReportProductReception)
-admin.site.register(ReportProductRemainStock)
+admin.site.register(ReportBeneficiary, ReportBeneficiaryAdmin)
+admin.site.register(ReportProductReception, ReportProductReceptionAdmin)
+admin.site.register(ReportProductRemainStock, ReportProductRemainStockAdmin)
 admin.site.register(Temporary)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
