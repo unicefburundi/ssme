d32f23cfafab64bfb5f777c9c36811e351d1b8d1
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2018-11-08 09:28:36 +0200

    state of the db on server

---
+++ ssme_activities/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ssme_activities', '0011_auto_20181107_1041'),
+        ('ssme_activities', '0009_auto_20160620_1613'),
+    ]
+
+    operations = [
+    ]
